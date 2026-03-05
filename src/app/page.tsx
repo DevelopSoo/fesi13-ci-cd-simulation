8e8f71d0992987d432ccac30aa3da6c906b7043a
DevelopSoo <69490984+DevelopSoo@users.noreply.github.com>
2026-03-06 00:39:08 +0900

    수동 배포

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <div>첫 배포</div>;
+// src/app/page.tsx
+
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div>
+      <div>한국어: 안녕</div>
+      <div>영어: {message}</div>
+    </div>
+  );
 }
